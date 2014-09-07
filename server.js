9e66be4fbe83fd310d0369698bc60a13e4810e6d
Josh Newman <josh.newman4390@gmail.com>
2014-09-07 19:32:13 +1000

    Server is now a basic static web server

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-var express = require('express');
+//-- Basic static web server
+//-- https://gist.github.com/rpflorence/701407
 
-// Constants
-var PORT = 8080;
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8888;
 
-// App
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
-});
+http.createServer(function(request, response) {
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
