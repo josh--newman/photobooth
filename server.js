c119d3db8a71595409900e11b32656a2c4519319
Josh Newman <josh.newman4390@gmail.com>
2014-09-18 14:31:22 +1000

    Sets up sockets correctly on server side

--- server.js
+++ server.js
@@ -2,16 +2,15 @@
 //-- https://gist.github.com/rpflorence/701407
 
 var PORT = 8080;
-var app = require("express")();
+var express = require("express");
+var app = express();
 var server = require("http").Server(app);
-var io = require('socket.io').listen(server);
+var io = require("socket.io")(server);
+var fs = require("fs");
 
-app.get("/", function(req, res) {
-  // res.sendfile(__dirname + "/css/styles.css");
-  res.sendfile(__dirname + "/index.html");
-});
+app.use(express.static(__dirname));
 
-var server = app.listen(PORT, function() {
+server.listen(PORT, function() {
   console.log("Listening on port %d", server.address().port);
 });
 
@@ -20,26 +19,28 @@ var gphoto2 = require("gphoto2");
 var GPhoto = new gphoto2.GPhoto2();
 var camera;
 GPhoto.list(function(list) {
-  if (list.length === 0) return;
+  if (list.length === 0) {
+    console.log("Cannot find camera!");
+    return;
+  }
   camera = list[0];
   console.log("Found", camera.model);
 });
 
 //-- Socket.io commands
-io.set('log level', 1); // reduce logging
-
 io.on("connection", function(socket) {
   //-- Take the photo and save to drive
   socket.on('takePhoto', function() {
-
-    console.log("Taking photo...");
-    camera.takePicture({
-      targetPath: "/tmp/foo.XXXXX"
-    }, function(er, tmpname) {
-      console.log("tmpname: " + tmpname);
-      fs.renameSync(tmpname, __dirname + "/photos/picture.jpg")
-      console.log("Done");
-    });
+    if (camera) {
+      console.log("Taking photo...");
+      camera.takePicture({
+        targetPath: "/tmp/foo.XXXXX"
+      }, function(er, tmpname) {
+        console.log("tmpname: " + tmpname);
+        fs.renameSync(tmpname, __dirname + "/photos/picture.jpg");
+        console.log("Done");
+      });
+    }
 
   });
 });
